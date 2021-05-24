c41d16ffef13cb9e1e8416d76fbfa51d05c114d4
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2021-05-24 19:15:36 -0400

    Update EntityPage.tsx

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -91,10 +91,6 @@ const serviceEntityPage = (
       {cicdContent}
     </EntityLayout.Route>
 
-    <EntityLayout.Route path="/github-actions" title="GitHub Actions">
-      <EntityGithubActionsContent />
-    </EntityLayout.Route>
-
     <EntityLayout.Route path="/api" title="API">
       <Grid container spacing={3} alignItems="stretch">
         <Grid item md={6}>
